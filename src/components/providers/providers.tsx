5db016965840c504f9d37e56c510601e3e669538
Vítor Oliveira <caulicons@gmail.com>
2024-12-10 16:56:38 -0300

    #9 feat: integrate Toaster component into App; remove unused ToastProvider and Toast from Providers; clean up toast-related code

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { ThemeProvider } from "./theme-provider";
-import { ToastProvider } from "../atoms/toast";
-import { Toast } from "@radix-ui/react-toast";
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,10 +9,7 @@ const Providers: React.FC<ProvidersProps> = ({ children }) => {
   return (
     // Add your providers here
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ToastProvider>
-        {children}
-        <Toast />
-      </ToastProvider>
+      {children}
     </ThemeProvider>
   );
 };
