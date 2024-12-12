02039445db73d6d8db10745f8860f03c5c3133ea
Vítor Oliveira <caulicons@gmail.com>
2024-12-12 08:51:59 -0300

    #17 feat: implement tilt effect for user card component; add useTiltEffect hook for mouse interaction

--- src/components/molecules/userCard/userCardList.tsx
+++ src/components/molecules/userCard/userCardList.tsx
@@ -2,6 +2,7 @@ import { UserCardScheme } from "../../../schemes/user/userCard.schema";
 import { Avatar, AvatarFallback, AvatarImage } from "../../atoms/avatar";
 import { Card, CardContent, CardHeader } from "../../atoms/card";
 import { Typography } from "../../atoms/typography";
+import useTiltEffect from "../../../hooks/use-tilt-effect";
 
 type UserCardListProps = Readonly<{
   user: UserCardScheme;
@@ -9,11 +10,18 @@ type UserCardListProps = Readonly<{
 }>;
 
 const UserCardList = ({ user, onClick }: UserCardListProps) => {
+  const { onMouseLeave, onMouseMove, rotate } = useTiltEffect();
   return (
     <Card
       key={user.id}
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
       className="flex flex-col items-center justify-between text-center  gap-3 min-w-[300px]  rounded-xl shadow-2xl   bg-white/[1%] border-none backdrop-blur-sm"
       onClick={onClick}
+      style={{
+        transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+        transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+      }}
     >
       <CardHeader>
         <Avatar className="h-24 w-24">
