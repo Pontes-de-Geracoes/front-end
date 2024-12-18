a94ad6a0b7a2810e0a2ea7b27bf433c2989e026b
Vítor Oliveira <caulicons@gmail.com>
2024-12-18 15:45:20 -0300

    #55 feat: Add vite-plugin-sitemap for improved sitemap generation and remove old robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
